d33fc4c80f0b5779c06b413027a0291dbd83feb6
Wachira-GK <godmarklee@gmail.com>
2022-08-17 00:10:40 +0300

    calc

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+    <a className='back' href="https://gkapps.netlify.app/">Back to projects</a>
+    
     <App />
+    
   </React.StrictMode>
 );
