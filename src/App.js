e8aba86d8a6838e22132161d0916b218bde7f68b
Wachira-GK <godmarklee@gmail.com>
2022-08-19 21:01:33 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -3,34 +3,65 @@ import {useState} from 'react';
 
 
 function App() {
-  const [calc, setCalc] = useState("");
-  const [result, setResult] = useState("");
+    const [calc, setCalc] = useState("");
+    const [result, setResult] = useState("");
 
-  const ops = ['/','*','+','-','.'];
-  const updateCalc = value => {
-    setCalc(calc + value);
-
-  }
-
-  const createDigits = () =>{
-    const digits = [];
-
-    for(let i = 1; i <10; i++ ) {
-      digits.push (
-        <button  
-            onClick={() => updateCalc(i.toString())} 
-                  key={i}>
-                    {i} 
-        </button>
-      )
+    const ops = ['/','*','+','-','.'];
+    const updateCalc = value => {
+		
+		if( 
+			ops.includes(value) && calc === '' ||
+			ops.includes(value) && ops.includes(calc.slice(-1))
+		){
+			return ;
+		}
+		
+      setCalc(calc + value);
+		
+			if(!ops.includes(value)){
+				setResult(eval(calc + value).toString());
+			}
+		
+		
+		
     }
-    return digits;
-  }
+
+    const createDigits = () =>{
+      const digits = [];
+
+      for(let i = 1; i <10; i++ ) {
+        digits.push (
+          <button  
+              onClick={() => updateCalc(i.toString())} 
+                    key={i}>
+                      {i} 
+          </button>
+        )
+      }
+      return digits;
+    }
+
+      const calculate = () => {
+        setCalc(eval(calc).toString());
+      }
+
+      const deleteLast = () => {
+        if (calc == '') {
+          return;
+        }
+        const value = calc.slice(0,-1);
+
+        setCalc(value);
+      }
+      
+
+
   return (
     <div className= "App">
       <div className="calculator">
             <div className="display">
-               {result ? <span>(0)</span> : '' } 
+               {result ? <span>({result})</span> : '' } 
+              &nbsp;
                {calc || "0"}
    
    
@@ -43,7 +74,7 @@ function App() {
                 <button onClick={() => updateCalc('-')}>-</button>
                 <button onClick={() => updateCalc('+')}>+</button>
                 
-                <button>DEL</button>
+                <button onClick={deleteLast}>DEL</button>
                 
             </div>
 
@@ -53,7 +84,7 @@ function App() {
               <button onClick={() => updateCalc ('.')}>.</button>
 
 
-              <button>=</button>
+              <button onClick={calculate}>=</button>
             </div>
 
 
